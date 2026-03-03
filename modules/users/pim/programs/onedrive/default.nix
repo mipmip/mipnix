cbca7de2c3edc92e05c96e1e07ffb002cf36d756
Pim Snel <post@pimsnel.com>
2026-03-03 15:59:46 +0100

    auto run after nixos-rebuild switch

--- modules/users/pim/programs/onedrive/default.nix
+++ modules/users/pim/programs/onedrive/default.nix
@@ -4,22 +4,22 @@ inputs,
 }:
 {
   flake.modules.homeManager.pim-onedrive = {
-    programs.onedrive = {
-      enable = true;
-      settings =   {
-        check_nomount = "false";
-        check_nosync = "false";
-        classify_as_big_delete = "1000";
-        cleanup_local_files = "false";
-        sync_business_shared_items = "true";
-        disable_notifications = "false";
-        no_remote_delete = "false";
-        rate_limit = "0";
-        resync = "false";
-        skip_dotfiles = "false";
-      };
-
-    };
+    #    programs.onedrive = {
+    #      enable = true;
+    #      settings =   {
+    #        check_nomount = "false";
+    #        check_nosync = "false";
+    #        classify_as_big_delete = "1000";
+    #        cleanup_local_files = "false";
+    #        sync_business_shared_items = "true";
+    #        disable_notifications = "false";
+    #        no_remote_delete = "false";
+    #        rate_limit = "0";
+    #        resync = "false";
+    #        skip_dotfiles = "false";
+    #      };
+    #
+    #    };
   };
 }
 
