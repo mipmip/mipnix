4c95ab57f2c79a44430e166d6c209aad9fc47308
Pim Snel <post@pimsnel.com>
2026-03-09 14:58:16 +0100

    auto run after home-manager switch

--- packages/mipvim/config/plugins/utils/custom_linny.nix
+++ packages/mipvim/config/plugins/utils/custom_linny.nix
@@ -7,31 +7,36 @@
       src = pkgs.fetchFromGitHub {
         owner = "linden-project";
         repo = "linny.vim";
-        rev = "cef962e96ccff85e25aa911f2e20fa5daf08b3fd";
-        hash = "sha256-jIXNclctwpDKfGP4dh5YvrXE9sXN8SWfcS75uspfAYM=";
+        rev = "e5e4689c29f303b8a5a45b16b71e36647d037363";
+        hash = "sha256-iMOrAcGRHJBIQCg5XDMkFG6n6gjuLkguCmUKBEDjQLc=";
       };
+      nvimSkipModule = [
+        "linny"
+        "linny.menu"
+        "linny.menu.init"
+      ];
     })
 
   ];
 
-  extraConfigLua =
-    # lua
-    ''
-      local f=io.open( os.getenv( "HOME" ) .. "/.i-am-second-brain","r")
-
-      if f~=nil then
-        io.close(f)
-
-        vim.g.linny_open_notebook_path = vim.env.HOME .. '/secondbrain'
-
-        vim.g.linny_menu_display_docs_count = 1
-        vim.g.linny_menu_display_taxo_count = 1
-        vim.g.linnycfg_setup_autocommands = 1
-
-        vim.cmd [[
-          let g:linny_wikitags_register = {}
-        ]]
-        vim.fn['linny#Init']()
-      end
-    '';
+  #  extraConfigLua =
+  #    # lua
+  #    ''
+  #      local f=io.open( os.getenv( "HOME" ) .. "/.i-am-second-brain","r")
+  #
+  #      if f~=nil then
+  #        io.close(f)
+  #
+  #        vim.g.linny_open_notebook_path = vim.env.HOME .. '/secondbrain'
+  #
+  #        vim.g.linny_menu_display_docs_count = 1
+  #        vim.g.linny_menu_display_taxo_count = 1
+  #        vim.g.linnycfg_setup_autocommands = 1
+  #
+  #        vim.cmd [[
+  #          let g:linny_wikitags_register = {}
+  #        ]]
+  #        vim.fn['linny#Init']()
+  #      end
+  #    '';
 }
