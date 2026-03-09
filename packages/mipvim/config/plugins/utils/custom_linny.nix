df55640040f263403a268ecd20988ee826adad84
Pim Snel <post@pimsnel.com>
2026-03-09 14:59:56 +0100

    auto run after home-manager switch

--- packages/mipvim/config/plugins/utils/custom_linny.nix
+++ packages/mipvim/config/plugins/utils/custom_linny.nix
@@ -19,24 +19,24 @@
 
   ];
 
-  #  extraConfigLua =
-  #    # lua
-  #    ''
-  #      local f=io.open( os.getenv( "HOME" ) .. "/.i-am-second-brain","r")
-  #
-  #      if f~=nil then
-  #        io.close(f)
-  #
-  #        vim.g.linny_open_notebook_path = vim.env.HOME .. '/secondbrain'
-  #
-  #        vim.g.linny_menu_display_docs_count = 1
-  #        vim.g.linny_menu_display_taxo_count = 1
-  #        vim.g.linnycfg_setup_autocommands = 1
-  #
-  #        vim.cmd [[
-  #          let g:linny_wikitags_register = {}
-  #        ]]
-  #        vim.fn['linny#Init']()
-  #      end
-  #    '';
+  extraConfigLua =
+    # lua
+    ''
+      local f=io.open( os.getenv( "HOME" ) .. "/.i-am-second-brain","r")
+
+      if f~=nil then
+        io.close(f)
+
+        vim.g.linny_open_notebook_path = vim.env.HOME .. '/secondbrain'
+
+        vim.g.linny_menu_display_docs_count = 1
+        vim.g.linny_menu_display_taxo_count = 1
+        vim.g.linnycfg_setup_autocommands = 1
+
+        vim.cmd [[
+          let g:linny_wikitags_register = {}
+        ]]
+        vim.fn['linny#Init']()
+      end
+    '';
 }
