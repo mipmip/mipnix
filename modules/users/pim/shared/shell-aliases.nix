bb95c4f4b9076fed1738f360c663f83b09cd001a
Pim Snel <post@pimsnel.com>
2026-03-16 13:20:15 +0100

    auto run after home-manager switch

--- modules/users/pim/shared/shell-aliases.nix
+++ modules/users/pim/shared/shell-aliases.nix
@@ -26,13 +26,21 @@ inputs,
           vim = "nvim";
           signal = ''signal-desktop --password-store="gnome-libsecret"'';
 
-          t = lib.mkDefault "tmux a || smug start lobby && smug start sudo && smug start nixos && smug start tekst";
+
 
           lin = "vim -c LinnyStart";
 
           tn = "tmux new -d -s";
+          t = lib.mkDefault "tmux a || smug start lobby && smug start sudo && smug start nixos && smug start tekst";
           tmxa = "tmux unbind C-a && tmux set-option -g prefix C-a && tmux bind-key C-a send-prefix";
           tmxb = "tmux unbind C-b && tmux set-option -g prefix C-b && tmux bind-key C-b send-prefix";
+
+          twn = ''
+          tmux rename-window "''$(basename "''$PWD")
+          '';
+
+
+
           mip = "WEBKIT_DISABLE_DMABUF_RENDERER=1 mip";
 
           smugs = lib.mkDefault "smug && smug start sudo && smug start nixos && smug start lobby";
