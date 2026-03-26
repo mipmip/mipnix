c483d6a7ad3d9b50db12f0a89240d9d5cce2e30c
Pim Snel <post@pimsnel.com>
2026-03-26 14:29:27 +0100

    auto run after home-manager switch

--- packages/mipvim/config/plugins/utils/custom_plugins.nix
+++ packages/mipvim/config/plugins/utils/custom_plugins.nix
@@ -33,44 +33,17 @@
       src = pkgs.fetchFromGitHub {
         owner = "mipmip";
         repo = "tmux-sendit.nvim";
-        rev = "d50660219ae39fa659a8d0b6644ef2f2df717f42";
-        hash = "sha256-rVqnQU8pdDUaN+uRAbc6nXMmZytRQhsOpyhedbQFDT0=";
+        rev = "37f5eb7d4225e835b82f6135f69d63b2dfa6066a";
+        hash = "sha256-F1uYsyIcLjGE79xIOC7OR+1wR3X+OS7/gzIj5gZZ0Mg=";
       };
     })
-
-
   ];
 
-    extraConfigLua =
-      ''
-        require('sendit').setup({
-          -- opts = {},
-        })
-
-        require('claudecode').setup({
-          -- opts = {},
-          keys = {
-            { "<leader>a", nil, desc = "AI/Claude Code" },
-            { "<leader>ac", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
-            { "<leader>af", "<cmd>ClaudeCodeFocus<cr>", desc = "Focus Claude" },
-            { "<leader>ar", "<cmd>ClaudeCode --resume<cr>", desc = "Resume Claude" },
-            { "<leader>aC", "<cmd>ClaudeCode --continue<cr>", desc = "Continue Claude" },
-            { "<leader>am", "<cmd>ClaudeCodeSelectModel<cr>", desc = "Select Claude model" },
-            { "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
-            { "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
-            {
-              "<leader>as",
-              "<cmd>ClaudeCodeTreeAdd<cr>",
-              desc = "Add file",
-              ft = { "NvimTree", "neo-tree", "oil", "minifiles", "netrw" },
-            },
-            -- Diff management
-            { "<leader>aa", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
-            { "<leader>ad", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
-          },
-        })
-
-      '';
+  extraConfigLua = ''
+    require('sendit').setup({
+      -- opts = {},
+    })
+  '';
 
 
 }
