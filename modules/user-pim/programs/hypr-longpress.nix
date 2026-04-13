835531538bfcf389410b2281370618d75f9a8714
Pim Snel <post@pimsnel.com>
2026-04-13 22:51:32 +0200

    auto run after home-manager switch

--- modules/user-pim/programs/hypr-longpress.nix
+++ modules/user-pim/programs/hypr-longpress.nix
@@ -5,8 +5,8 @@
   flake.modules.homeManager.pim-hypr-longpress = { pkgs, ... }: {
     home.packages = [
       (pkgs.writeShellScriptBin "hypr-longpress" ''
-        export PATH="${pkgs.rofi}/bin:$PATH"
-        exec ${pkgs.python3.withPackages (ps: [ ps.evdev ])}/bin/python3 \
+        export PATH="${pkgs.rofi}/bin:${pkgs.libinput}/bin:$PATH"
+        exec ${pkgs.python3}/bin/python3 \
           ${./hyprland/scripts/hypr-longpress} "$@"
       '')
     ];
