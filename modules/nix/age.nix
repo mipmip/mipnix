dd9e4ccdcc4e5a5cc00e013841f6c1f361585223
Pim Snel <post@pimsnel.com>
2026-04-20 15:26:09 +0200

    auto run after nixos-rebuild switch

--- modules/nix/age.nix
+++ modules/nix/age.nix
@@ -2,11 +2,15 @@
   flake.modules.nixos.nix-age = { pkgs, ... }: {
 
     environment.systemPackages = [
-      inputs.agenix.packages."${pkgs.stdenv.hostPlatform.system}".default
+      (inputs.agenix.packages."${pkgs.stdenv.hostPlatform.system}".default.override {
+        ageBin = "${pkgs.rage}/bin/rage";
+      })
     ];
 
     imports = [
       inputs.agenix.nixosModules.default
     ];
+
+    age.ageBin = "${pkgs.rage}/bin/rage";
   };
 }
