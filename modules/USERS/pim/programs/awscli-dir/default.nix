70acc595d71186da34717e202d1b480c798482bc
Pim Snel <post@pimsnel.com>
2026-04-23 18:27:03 +0200

    auto run after home-manager switch

--- modules/USERS/pim/programs/awscli-dir/default.nix
+++ modules/USERS/pim/programs/awscli-dir/default.nix
@@ -11,8 +11,8 @@ inputs,
     other_profiles = /home/pim/.aws/other_accounts.json;
 
     aws_accounts = []
-          #++ builtins.fromJSON (lib.readFile other_profiles)
-      ++ builtins.fromJSON (lib.readFile technative_profiles);
+        ++ builtins.fromJSON (lib.readFile other_profiles)
+        ++ builtins.fromJSON (lib.readFile technative_profiles);
 
     # TODO SET THESE VALS in agenix
     groups = {
