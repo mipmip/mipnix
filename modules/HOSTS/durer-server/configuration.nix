734c31843352f4577220b4668909d55c79ae6ee3
Pim Snel <post@pimsnel.com>
2026-05-04 23:18:44 +0200

    auto run after nixos-rebuild switch

--- modules/HOSTS/durer-server/configuration.nix
+++ modules/HOSTS/durer-server/configuration.nix
@@ -67,7 +67,7 @@ in
         accounts = {
           authentication-enabled = true;
           registration.enabled = false;
-          require-sasl.enabled = false;
+          require-sasl.enabled = true;
           multiclient = {
             enabled = true;
             always-on = "opt-out";
