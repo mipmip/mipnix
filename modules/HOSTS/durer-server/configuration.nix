663f6b350672ca75f82b871cc471bb4a179ccf94
Pim Snel <post@pimsnel.com>
2026-05-05 11:51:17 +0200

    implement tuwunel

--- modules/HOSTS/durer-server/configuration.nix
+++ modules/HOSTS/durer-server/configuration.nix
@@ -36,13 +36,35 @@ in
     # --- Nginx (ACME webserver + health check) ---
     services.nginx = {
       enable = true;
+      clientMaxBodySize = "20m";
       virtualHosts."nuremberg.pimsnel.com" = {
         enableACME = true;
         forceSSL = true;
-        locations."/".return = ''200 "durer ok\n"'';
-        extraConfig = ''
-          default_type text/plain;
-        '';
+        locations."/" = {
+          return = ''200 "durer ok\n"'';
+          extraConfig = "default_type text/plain;";
+        };
+        locations."/_matrix/" = {
+          proxyPass = "http://localhost:6167";
+        };
+        locations."/.well-known/matrix/server" = {
+          return = ''200 '{"m.server":"nuremberg.pimsnel.com:443"}'
+          '';
+          extraConfig = ''
+            default_type application/json;
+            add_header Access-Control-Allow-Origin *;
+          '';
+        };
+        locations."/.well-known/matrix/client" = {
+          return = ''200 '{"m.homeserver":{"base_url":"https://nuremberg.pimsnel.com"}}'
+          '';
+          extraConfig = ''
+            default_type application/json;
+            add_header Access-Control-Allow-Origin *;
+            add_header Access-Control-Allow-Methods "GET, POST, PUT, DELETE, OPTIONS";
+            add_header Access-Control-Allow-Headers "X-Requested-With, Content-Type, Authorization";
+          '';
+        };
       };
     };
 
@@ -95,6 +117,19 @@ in
             ];
           };
         };
+      };
+    };
+
+    # --- Tuwunel Matrix ---
+    services.matrix-tuwunel = {
+      enable = true;
+      settings.global = {
+        server_name = "nuremberg.pimsnel.com";
+        port = [ 6167 ];
+        allow_registration = false;
+        allow_encryption = true;
+        allow_federation = false;
+        trusted_servers = [];
       };
     };
 
