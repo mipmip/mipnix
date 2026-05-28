68bcbe807d76d56f3c81bf5906a3d99c4e53277f
Pim Snel <post@pimsnel.com>
2026-05-28 20:12:14 +0200

    enable mosh on all servers

--- modules/ROLES/nixos-server.nix
+++ modules/ROLES/nixos-server.nix
@@ -6,6 +6,7 @@ inputs,
   flake.modules.nixos.role-server = {
 
     boot.kernelParams = [ "consoleblank=60" ];
+    programs.mosh.enable = true;
 
     imports = with inputs.self.modules.nixos; [
     ];
