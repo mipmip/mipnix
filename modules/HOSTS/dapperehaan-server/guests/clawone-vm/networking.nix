9f914ddc1f8aea5286482f2c84abed1b2a0b5140
Pim Snel <post@pimsnel.com>
2026-05-28 11:41:23 +0200

    fix clawone-vm: bump mem to 2049 and match ether type for networking
    QEMU hangs with exactly 2048 MB RAM. Guest network interface name
    is not eth0 in microvm, so match on Type=ether instead.

--- modules/HOSTS/dapperehaan-server/guests/clawone-vm/networking.nix
+++ modules/HOSTS/dapperehaan-server/guests/clawone-vm/networking.nix
@@ -11,8 +11,8 @@ in
 
     systemd.network.enable = true;
 
-    systemd.network.networks."20-eth0" = {
-      matchConfig.Name = "eth0";
+    systemd.network.networks."20-lan" = {
+      matchConfig.Type = "ether";
       addresses = [{ Address = "10.0.100.2/24"; }];
       routes = [{ Gateway = "10.0.100.1"; }];
       dns = [ "1.1.1.1" "8.8.8.8" ];
