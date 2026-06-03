4cca675713281119121c959ee642532ba89a6dcf
Pim Snel <post@pimsnel.com>
2026-06-03 22:06:18 +0200

    auto run after home-manager switch

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
   inputs = {
 
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11"; # GNOME 49
+    nixpkgs-old-stable.url = "github:NixOS/nixpkgs/nixos-25.11"; # GNOME 49
     nixpkgs-mama.url = "github:NixOS/nixpkgs/nixos-25.11"; # GNOME 49
 
     nix-index-database.url = "github:nix-community/nix-index-database";
