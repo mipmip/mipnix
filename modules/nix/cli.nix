821840f5bf2e4d4b15b70dd51764ad0d43d7be9e
Pim Snel <post@pimsnel.com>
2026-06-09 12:03:25 +0200

    auto run after home-manager switch

--- modules/nix/cli.nix
+++ modules/nix/cli.nix
@@ -11,17 +11,6 @@
       keep-derivations = true
       keep-failed = true
       '';
-
-      settings = {
-        extra-substituters = [
-          "https://walker.cachix.org"
-          "https://walker-git.cachix.org"
-        ];
-        extra-trusted-public-keys = [
-          "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
-          "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
-        ];
-      };
     };
 
   };
