dd8c8f9feb0199646af149b46949ab11778ddb5b
Pim Snel <post@pimsnel.com>
2026-06-29 13:55:13 +0200

    auto run after nixos-rebuild switch

--- modules/programs/grannyos/granny.nix
+++ modules/programs/grannyos/granny.nix
@@ -3,6 +3,7 @@
   flake.modules.nixos.granny = { pkgs, ... }: {
 
     environment.systemPackages = with pkgs; [
+
       inputs.rme.packages."${pkgs.stdenv.hostPlatform.system}".default
 
       vim
@@ -11,15 +12,14 @@
 
       wget
 
-      firefox
-      flare-signal
+      moving.firefox
+      #flare-signal
 
-      mobing.fastmail-desktop
-      moving.bitwarden-desktop
-      moving.bitwarden-desktop
-      moving.newelle
-      moving.signal-desktop
-      moving.rustdesk
+      #moving.fastmail-desktop
+      #moving.bitwarden-desktop
+      #moving.newelle
+      #moving.signal-desktop
+      #moving.rustdesk
 
       nebula
 
@@ -35,8 +35,8 @@
 
       ghostty
 
-      gnome-tweaks
-      gpaste
+      #gnome-tweaks
+      #gpaste
     ];
   };
 }
