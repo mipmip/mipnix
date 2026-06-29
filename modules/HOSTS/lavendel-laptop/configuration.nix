3b9cd88edabf5448a2ee4a5e5fef5b00641d41a5
Pim Snel <post@pimsnel.com>
2026-06-29 12:48:52 +0200

    changes for mama

--- modules/HOSTS/lavendel-laptop/configuration.nix
+++ modules/HOSTS/lavendel-laptop/configuration.nix
@@ -5,7 +5,6 @@ let
 in
 
 {
-
   flake.nixosConfigurations = {
 
     lavendel = self.lib.makeNixos {
@@ -13,14 +12,6 @@ in
       system = "x86_64-linux";
       channel = inputs.nixpkgs-mama;
     };
-  };
-
-  flake.homeConfigurations = {
-
-    "pim@arcana-one" = self.lib.makeHomeConf {
-      hostname = "arcana-one";
-    };
-
   };
 
   flake.homeConfigurations = {
@@ -37,54 +28,50 @@ in
 
   flake.modules.nixos.lavendel = { config, pkgs, ... } : {
 
-    services.displayManager.gdm.enable = true;
-    services.desktopManager.gnome.enable = true;
-
-    services.displayManager.defaultSession = "gnome";
-
-
     imports = with inputs.self.modules.nixos; [
 
-      channel-annemarie
-
       system-default
-      role-nebula-node
-
-      inputs.nixos-hardware.nixosModules.framework-12-13th-gen-intel
-
-      framework-misc
-      hardware-chipsailing-fingerprint
-      role-nebula-node
-      networking-nebula
-
-      networking-wifi
+      channel-annemarie
 
       user-annemarie
       role-desktop-annemarie
       system-trusted-annemarie
 
+      inputs.nixos-hardware.nixosModules.framework-12-13th-gen-intel
+      framework-misc
+      hardware-chipsailing-fingerprint
+
+      role-nebula-node
+      networking-nebula
+      networking-wifi
+
     ];
 
-    # Bootloader.
+    console.keyMap = "nl";
+
+    security.rtkit.enable = true;
+
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
-
     boot.initrd.luks.devices."luks-46b0982e-ca31-46d1-a008-497efd7b931f".device = "/dev/disk/by-uuid/46b0982e-ca31-46d1-a008-497efd7b931f";
 
-    # Enable networking
-    networking.networkmanager.enable = true;
+    programs.mosh.enable = true;
 
+    networking.networkmanager.enable = true;
+    networking.useDHCP = lib.mkDefault true;
+
+    services.displayManager.gdm.enable = true;
+    services.desktopManager.gnome.enable = true;
+    services.displayManager.defaultSession = "gnome";
+
+    services.power-profiles-daemon.enable = true;
+    services.fwupd.enable = true;
     services.xserver.xkb = {
       layout = "nl";
       variant = "us";
     };
-
-    console.keyMap = "nl";
-
     services.printing.enable = true;
-
     services.pulseaudio.enable = false;
-    security.rtkit.enable = true;
     services.pipewire = {
       enable = true;
       alsa.enable = true;
@@ -92,11 +79,7 @@ in
       pulse.enable = true;
     };
 
-    system.stateVersion = "25.11"; # Did you read the comment?
-    networking.useDHCP = lib.mkDefault true;
-    services.power-profiles-daemon.enable = true;
-    services.fwupd.enable = true;
-
+    system.stateVersion = "25.11";
 
   };
 
